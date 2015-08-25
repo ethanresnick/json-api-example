135a28fdeaa08ab02a04b9e6c2e64253bc6c360e
Ethan Resnick <ethan.resnick@gmail.com>
2015-08-25 14:57:51 -0700

    People model: Add parent relationship
    Offers a test for https://github.com/ethanresnick/json-api/issues/49

--- src/models/person.js
+++ src/models/person.js
@@ -1,9 +1,11 @@
 'use strict';
-var mongoose   = require('mongoose');
+var mongoose   = require('mongoose')
+  , ObjectId   = mongoose.Schema.Types.ObjectId;
 
 var schema = mongoose.Schema({
   name: String,
   email: {type: String, lowercase: true},
+  parent: {ref:'Person', type: ObjectId},
   gender: {
     type: String,
     enum: ['male', 'female', 'other']
